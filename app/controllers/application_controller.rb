4241b398818552104066a1cd7351589615c85275
Dr Nic Williams <drnicwilliams@gmail.com>
2010-02-25 10:00:26 +1000

    redirect to forms.mocra.com if using alternate domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :check_uri
+
   helper :all # include all helpers, all the time
   # protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+
+
+  def check_uri
+    if Rails.env.production?
+      redirect_to request.protocol + "forms.mocra.com" + request.request_uri if !/^forms.mocra.com/.match(request.host)
+    end
+  end
 end
